8d7f529443e79f5e239e0ce8fbea218d2a4c5ec8
phaunus <1057021+ph4un00b@users.noreply.github.com>
2023-05-25 23:17:47 -0600

    testing routes, not-working

--- entries.ts
+++ entries.ts
@@ -3,9 +3,13 @@ import { defineEntries } from "waku/server";
 export default defineEntries(
   // getEntry
   async (id) => {
+    console.log({ id });
     switch (id) {
       case "App":
         return import("./src/App.js");
+      // not-working this way
+      case "LangchainPDF":
+        return import("./src/pages/pdf.js");
       case "InnerApp":
         return import("./src/InnerApp.js");
       default:
@@ -25,6 +29,10 @@ export default defineEntries(
           ["InnerApp", { count: 4 }, true],
           ["InnerApp", { count: 5 }, true],
         ],
+      },
+      // not-working this way
+      "/pdf": {
+        elements: [["LangchainPDF", { name: "phau!" }]],
       },
     };
   }
